77f8004eae301e271c58d11a456094cc74eed992
Harsh10 <harshsahani078@gmail.com>
2024-07-23 20:51:39 +0530

    Db connection established

---
+++ config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+exports.dbConnection= ()=>{
+
+    try{
+        mongoose.connect(process.env.DB_URL)
+        .then(()=> console.log("Db connection established"))
+        .catch( (e)=> console.log(e.message));
+    }
+    catch(e){
+        console.log(e.message)
+    }   
+}
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const server = express();
+const dbConnect = require('./config/database');
 
 require("dotenv").config();
 const Port = process.env.PORT;
+
+dbConnect.dbConnection();
 
 server.listen(Port);
