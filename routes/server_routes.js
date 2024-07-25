67aafb97e6ca5559677afd655fec3703852087dd
Harsh10 <harshsahani078@gmail.com>
2024-07-25 13:18:03 +0530

    Added liked post route

--- routes/server_routes.js
+++ routes/server_routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const createPost = require('../controler/createPost');
-const getPosts = require('../controler/getPosts')
+const getPosts = require('../controler/getPosts');
+const likePost = require('../controler/likePost')
 
 
 router.post('/posts/create',createPost.createPost);
 router.get('/posts',getPosts.getPosts);
+router.put('/likes/like/:id',likePost.likePost);
 
 module.exports=router;
