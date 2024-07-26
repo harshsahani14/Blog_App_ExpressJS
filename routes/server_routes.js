58fd6fbd99a940985a7775e7c05acb282a2d6fd7
Harsh10 <harshsahani078@gmail.com>
2024-07-26 11:11:50 +0530

    Unlike route added

--- routes/server_routes.js
+++ routes/server_routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const createPost = require('../controler/createPost');
 const getPosts = require('../controler/getPosts');
-const likePost = require('../controler/likePost')
+const likePost = require('../controler/likePost');
+const unLikePost = require('../controler/unlikePost')
 
 
 router.post('/posts/create',createPost.createPost);
 router.get('/posts',getPosts.getPosts);
 router.put('/likes/like/:id',likePost.likePost);
+router.put('/likes/unlike/:id',unLikePost.unLikePost);
 
 module.exports=router;
